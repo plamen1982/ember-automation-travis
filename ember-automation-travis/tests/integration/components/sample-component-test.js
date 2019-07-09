edbcfe16608dbfaa367a49f53d85bacdbe32490e
bilgeryahov <bayahov1@gmail.com>
2019-07-09 14:05:31 +0300

    Basic integration test for the sample component.

--- ember-automation-travis/tests/integration/components/sample-component-test.js
+++ ember-automation-travis/tests/integration/components/sample-component-test.js
@@ -3,24 +3,12 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | sample-component', function(hooks) {
+module('Integration | Component | sample-component', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
+  test('it renders', async function (assert) {
     await render(hbs`<SampleComponent />`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      <SampleComponent>
-        template block text
-      </SampleComponent>
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.equal(this.element.querySelector('h1').textContent.trim(), 'Hello!');
+    assert.equal(this.element.querySelector('p').textContent.trim(), 'I am a test!');
   });
 });
